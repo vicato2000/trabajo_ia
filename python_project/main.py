446549d380679ef385e1e522c0c1b088b157a7fb
Vicente Cambron Tocados <vicentect10@gmail.com>
2022-06-05 13:20:24 +0200

    05/06/22-13:20

--- python_project/main.py
+++ python_project/main.py
@@ -1,16 +1,16 @@
 from prueba_seleccion_vocabulario import *
 from utils.reader import *
+from utils.clean_code import *
+import os
 
 if __name__ == '__main__':
-    email_list = read_emails('/home/vicato/IA/trabajo_ia/Enron-Spam/legítimo')
 
-    # file = ''
-    #
-    # for i in range(0, len(email_list) - 1):
-    #     file += email_list[i][1] + '\n'
-    #     file += '---------------------------------------------------------------------\n'
-    #
-    # f = open('../documentos_prueba/read_emails_prueba.txt', 'w')
-    # f.write(file)
+    email_list = read_emails('/home/vicato/IA/trabajo_ia/Enron-Spam/no_deseado')
 
-    extract_vocabulary(email_list)
+    file = ''
+
+    for i in range(0, len(email_list) - 1):
+        file += '{}'.format(denoise_text('{}'.format(email_list[i]))) + '\n'
+
+    f = open('/home/vicato/IA/trabajo_ia/python_project/documentos_prueba/read_emails_prueba.txt', 'w')
+    f.write(file)
--- python_project/utils/reader.py
+++ python_project/utils/reader.py
@@ -1,33 +1,34 @@
 from email.parser import Parser
-from email.policy import default
+from email import policy
+import email as em
 import os
+import json
 
 
 def read_email(email_path):
-    email = open(email_path, 'r')
+    with open(email_path, 'r', encoding='latin1') as email:
+        msg = em.message_from_string(email.read(), policy=policy.default)
 
-    headers = Parser(policy=default).parsestr(email.read())
-
-    return 'body-{}'.format(email_path.split('/')[-1]), headers.get_payload()
+        if msg.is_multipart():
+            return msg.get_body()
+        else:
+            return msg.get_payload()
 
 
 def read_emails(emails_folder_path):
     folder_files_list = [int(file) for file in os.listdir(emails_folder_path)]
     folder_files_list.sort()
 
-    print(folder_files_list)
-
     return [read_email('{}/{}'.format(emails_folder_path, e)) for e in folder_files_list]
 
 
 if __name__ == '__main__':
-    email_list = read_emails('/home/vicato/IA/trabajo_ia/Enron-Spam/legítimo')
+    email_list = read_emails('/home/vicato/IA/trabajo_ia/Enron-Spam/no_deseado')
 
     file = ''
 
     for i in range(0, len(email_list) - 1):
-        file += email_list[i][1] + '\n'
-        file += '---------------------------------------------------------------------\n'
+        file += '{}'.format(email_list[i]) + '\n'
 
     f = open('../documentos_prueba/read_emails_prueba.txt', 'w')
     f.write(file)
